b1cd6ceb41061c4ad0024fe9fca6dce3f4013e3e
huangyifeng <hyfmailsave@163.com>
2021-05-18 16:20:37 +0800

    上传springsecurity权限验证： 1.自定义登录页面jsp； 2.密码实现加密加盐

--- security-springboot/src/main/java/com/fengye/securityspringboot/controller/LoginController.java
+++ security-springboot/src/main/java/com/fengye/securityspringboot/controller/LoginController.java
@@ -1,5 +1,9 @@
 package com.fengye.securityspringboot.controller;
 
+import com.fengye.securityspringboot.dao.UserDao;
+import com.fengye.securityspringboot.model.UserDto;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,9 +15,35 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class LoginController {
+
+
     @RequestMapping(value = "/login-success",produces = {"text/plain;charset=UTF-8"})
     public String loginSuccess(){
-        return " 登录成功";
+        String username = getUsername();
+        return username + " 登录成功";
+    }
+
+    /**
+     * 获取当前登录用户名
+     * @return
+     */
+    public String getUsername(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        //如果未授权
+        if(!authentication.isAuthenticated()){
+            return null;
+        }
+
+        Object principal = authentication.getPrincipal();
+        String username = "";
+        if(principal instanceof UserDto){
+            username = ((UserDto) principal).getUsername();
+        }
+        else {
+            username = principal.toString();
+        }
+
+        return username;
     }
 
     /**
@@ -22,7 +52,8 @@ public class LoginController {
      */
     @GetMapping(value = "/r/r1",produces = {"text/plain;charset=UTF-8"})
     public String r1(){
-        return " 访问资源1";
+        String username = getUsername();
+        return username + " 访问资源1";
     }
 
     /**
@@ -31,6 +62,7 @@ public class LoginController {
      */
     @GetMapping(value = "/r/r2",produces = {"text/plain;charset=UTF-8"})
     public String r2(){
-        return " 访问资源2";
+        String username = getUsername();
+        return username + " 访问资源2";
     }
 }
--- security-springboot/src/test/java/com/fengye/securityspringboot/SecuritySpringbootApplicationTests.java
+++ security-springboot/src/test/java/com/fengye/securityspringboot/SecuritySpringbootApplicationTests.java
@@ -1,13 +1,23 @@
 package com.fengye.securityspringboot;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @SpringBootTest
-class SecuritySpringbootApplicationTests {
+@RunWith(SpringJUnit4ClassRunner.class)
+public class SecuritySpringbootApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void testBCryptPasswordEncoder() {
+        //对原始密码加密
+        String saltPassword = BCrypt.hashpw("123", BCrypt.gensalt());
+        System.out.println(saltPassword);
+        //检验原始密码和BCrypt密码是否一致，加密加盐后的值
+        boolean res = BCrypt.checkpw("123", "$2a$10$lVKKazLSJncZ5fPYV1TjEOKHliXt7Xo.IeTuFUktEcOHoqEigCSXu");
+        System.out.println(res);
     }
 
 }
